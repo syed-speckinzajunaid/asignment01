b378e7c1bda8f25f2f5c76bf81c7c6087bdf941f
kinzajunaid <syed junaid khalid010@gmail.com>
2022-04-23 08:14:29 -0700

    first commit

---
+++ app1.js
@@ -0,0 +1,18 @@
+
+
+
+ document.write('chapter no.1 ')
+ 
+alert("Hello World");
+
+alert("Error! Please enter a valid password <br>");
+
+ alert("Welcome to JS Land../nHapping  Coding!");
+ 
+  alert("Welcome to JS Land..");
+alert("Happing Coding!\nPrevent this Page fro creating additinal dailogs");
+
+ alert("Hello.. I can run JS through my web browser's console");
+
+
+
